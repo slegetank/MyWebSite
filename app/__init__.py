daeecbb3382c1f025130206f0d6a71edb6bd72a7
Shuai Zhao <shuai.zhao@wiimu.com>
2017-03-13 19:41:34 +0800

    支持子域名

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ import os
 db = SQLAlchemy()
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=None)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:%s@%s/mywebsite?charset=utf8' % (os.getenv('MYWEBSITE_DB_PASS'), os.getenv('MYWEBSITE_HOST'))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SERVER_NAME'] = 'slegetank.com'
 
     db.init_app(app)
 
@@ -19,10 +20,20 @@ def create_app(config_name):
     # blog
     from blog import blog
     if config_name == "debug":
+        os.environ['MYWEBSITE_HOST'] = "slegetank.com"
         app.register_blueprint(blog, url_prefix='/blogs')
     elif config_name == "release":
         app.url_map.default_subdomain="www"
-        app.config['SERVER_NAME'] = 'slegetank.com'
+        app.static_url_path = "/static"
+        app.static_folder = "static"
+        app.add_url_rule(app.static_url_path + '/<path:filename>',
+                         endpoint='static',
+                         view_func=app.send_static_file,
+                         subdomain="static")
         app.register_blueprint(blog, subdomain="blog")
+
+        os.environ['MYWEBSITE_HOST'] = "localhost"
+
+    os.environ['MYWEBSITE_DB_PASS'] = "Wiimu123456"
 
     return app
--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 from app import create_app
 
-app = create_app('debug')
+config = 'debug'
+app = create_app(config)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if config == 'debug':
+        app.run(debug=True)
+    else:
+        app.run(debug=False)
